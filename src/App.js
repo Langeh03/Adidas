4a59bb61af0d24f28a194da40dfce3cdb51fb880
Langeh03 <langehmohammed7@gmail.com>
2024-02-28 17:39:05 +0100

    creation and styling completion of about component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./Components/NavigationBar/Navigation"
+import About from "./Components/About/About";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
       <>
         <Nav/>
+        <About/>
         <Footer/>
       </>
   );
