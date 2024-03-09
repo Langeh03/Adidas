b05693750fd3276e89812f6b1c130f6da82f1494
Langeh03 <langehmohammed7@gmail.com>
2024-03-09 21:15:33 +0100

    creation of DB and update of assets

---
+++ src/DB/Brand.js
@@ -0,0 +1,24 @@
+const brand =[
+    {
+        img: "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_400,w_400/ub1_march_tcc_w_2_d_28fed18217.jpg",
+        title: "ICONIC FEELS. REFINED.",
+        description: "Ultraboost 1.0 connects comfort and classic minimalism. Refined. Reborn.",
+    },
+    {
+        img: "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_400,w_400/running_ss24_supernova_global_launch_hp_teaser_carousel_v2_d_4994b8c91f.jpg",
+        title: "SUPERCOMFORT. SUPERNOVA.",
+        description: "Experience maximum comfort with the new Supernova.",
+    },
+    {
+        img: "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_400,w_400/outdoor_ss24_terrex_free_hiker_global_anniversary_glpm_teasercarousel_d_8784288258.jpg",
+        title: "FREE HIKER 2.0",
+        description: "Go further in our most comfortable range of hiking shoes yet.",
+    },
+    {
+        img: "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_400,w_400/sportswear_ss24_energizing_the_sol_global_launch_dotcom_hp_tc_d_9efd0a8ef1.jpg",
+        title: "PRINT LOVER'S PARADISE",
+        description: "Chase the sun in our latest collaboration with FARM Rio.",
+    },
+];
+
+export default brand;
---
+++ src/DB/Products.js
@@ -0,0 +1,90 @@
+import heart from '../Assets/heart.svg'
+
+const products =[
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/e1ba91a12aef486680e5dbf046ac978f_9366/Samba_OG_Shoes_White_IF3814_01_standard.jpg",
+        title: "Samba OG Shoes",
+        category: "Shoes",
+        price: "$100",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/50571fa4268d4da19413bce5afacba73_9366/Ultraboost_1.0_Shoes_Brown_ID5886_01_standard.jpg",
+        title: "Ultraboost 1.0 Shoes",
+        category: "Shoes",
+        price: "$190",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/cbf96f70d8b54fb7a91c22b8d5edd07d_9366/Handball_Spezial_Shoes_Brown_IF6490_01_standard.jpg",
+        title: "Handball Spezial Shoes",
+        category: "Shoes",
+        price: "$110",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/9a1f70695afe4a058c4045bdb96ecec0_9366/Samba_OG_Shoes_White_IG1376_01_standard.jpg",
+        title: "Samba OG Shoes",
+        category: "Shoes",
+        price: "$150",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/0913ec2df25c48aaa66f04ed5051d4ab_9366/Gazelle_Shoes_Purple_IE0444_01_standard.jpg",
+        title: "Gazelle Shoes",
+        category: "Shoes",
+        price: "$100",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/974b9919ac5947af8f75b531eeafcf78_9366/Sambae_x_KSENIASCHNAIDER_Shoes_Beige_ID0444_01_standard.jpg",
+        title: "Sambae x KSENIASCHNAIDER Shoes",
+        category: "Shoes",
+        price: "$120",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/186872d7bb8445edaa5bf79de16759e6_9366/Samba_OG_Shoes_White_IE3437_01_standard.jpg",
+        title: "Samba OG Shoes",
+        category: "Shoes",
+        price: "$100",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/50d17ee47f604d78b963e3a932d7d5f8_9366/Ultraboost_1.0_Shoes_Blue_IE8483_01_standard.jpg",
+        title: "Ultraboost 1.0 Shoes",
+        category: "Shoes",
+        price: "$190",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/f704ba8f7ca6475d8892f9b3fcd40ded_9366/Gazelle_Bold_Shoes_Purple_IE0419_01_standard.jpg",
+        title: "Gazelle Bold Shoes",
+        category: "Shoes",
+        price: "$120",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/1708e68643bb4a0cba84783c1e4b91de_9366/Gazelle_Bold_Shoes_Black_IE0422_01_standard.jpg",
+        title: "Gazelle Bold Shoes",
+        category: "Shoes",
+        price: "$120",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/78cbcc36ed0d459cad4b6583957809a1_9366/Ultraboost_1.0_Shoes_White_IH7313_01_standard.jpg",
+        title: "Ultraboost 1.0 Shoes",
+        category: "Shoes",
+        price: "$190",
+        heart: {heart},
+    },
+    {
+        img: "https://assets.adidas.com/images/w_600,f_auto,q_auto/6378fc225526467893defbf908b10b41_9366/Campus_00s_Shoes_Pink_ID3173_01_standard.jpg",
+        title: "Campus 00s Shoes",
+        category: "Shoes",
+        price: "$110",
+        heart: {heart},
+    },
+];
+
+export default products;
